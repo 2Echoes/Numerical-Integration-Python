6f39033048d511220184b254b02e5da297b3669b
2Echoes <floric.slimani@live.fr>
2019-10-11 19:00:22 +0200

    Meth_Simpson_adapt tj en défaut

--- MethodeSimpson.py
+++ MethodeSimpson.py
@@ -1,23 +1,26 @@
-def Meth_Simpson(f,a,b,N): #f la fonction à intégrer, a la borne inf, b la borne sup et N le nb de pts d'integration
-    h = (b-a)/N
-    res = f(a)+f(b)
-    for i in range(1,N,2):
-        xk=a+i*h
-        res+= f(xk-h)+4*f(xk)+f(xk+h)
-    res*=h/3
+def Meth_Simpson(fonction,borne_min,borne_max,nb_intpoints):
+    #f la fonction à intégrer, a la borne inf, b la borne sup et N le nb de pts d'integration
+    step = (borne_max-borne_min)/nb_intpoints
+    res = fonction(borne_min)+fonction(borne_max)
+    for i in range(1,nb_intpoints,2):
+        curr_point=borne_min+i*step
+        res+= fonction(curr_point-step)+4*fonction(curr_point)+fonction(curr_point+step)
+    res*=step/3
     return res
 
-def error_Simpson(f,a,b,N): # Renvoie l'erreur commise pour la Meth_Simpson avec N points, f la fction, a et b les bornes inf et sup
+def error_Simpson(f,a,b,N): 
+    # Renvoie l'erreur commise pour la Meth_Simpson avec N points, f la fction, a et b les bornes inf et sup
     return (1/15)*(Meth_Simpson(f,a,b,N)-Meth_Simpson(f,a,b,int(N/2))) #préférable d'avoir N pair
 
-def Meth_Simpson_adapt(f,a,b,delta): #Intégration avec la méthode de Simpson où delta est la marge d'erreur maximale
+def Meth_Simpson_adapt(f,a,b,delta): 
+    #Intégration avec la méthode de Simpson où delta est la marge d'erreur maximale
     N=1000
     error=1.0E38 # Initialisation à l'infini
     I1 = Meth_Simpson(f,a,b,N)
     while error>delta:
         N*=2
         h=(b-a)/N
-        I2=(I1/2)*(3/h)
+        I2=(I1/2)*3/(2*h)
         for i in range(1,N,2):
             I2+=f(a+i*h)
         I2*=h/3
@@ -30,4 +33,4 @@ from math import sin,pi
 def g(c):
     return c**2*sin(c)
 
-print(Meth_Simpson(g,0,pi,1000),'\n',pi**2-4)
+print(Meth_Simpson_adapt(g,0,pi,1.0E-6),'\n',pi**2-4)
